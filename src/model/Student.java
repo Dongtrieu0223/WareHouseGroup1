85a3e20d07dd6933428b67021ddfc1c933e77935
= <=kietvvan0205@gmail.com>
2020-07-05 20:04:43 +0700

    create class Student

---
+++ src/model/Student.java
@@ -0,0 +1,78 @@
+package model;
+
+import java.sql.Date;
+
+public class Student {
+	private int mssv, idLog;
+	private String ho, ten, lop, tenlop, sdt, email, quequan, ghichu;
+	private Date dob;
+	
+	public int getMssv() {
+		return mssv;
+	}
+	public void setMssv(int mssv) {
+		this.mssv = mssv;
+	}
+	public int getIdLog() {
+		return idLog;
+	}
+	public void setIdLog(int idLog) {
+		this.idLog = idLog;
+	}
+	public String getHo() {
+		return ho;
+	}
+	public void setHo(String ho) {
+		this.ho = ho;
+	}
+	public String getTen() {
+		return ten;
+	}
+	public void setTen(String ten) {
+		this.ten = ten;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public String getLop() {
+		return lop;
+	}
+	public void setLop(String lop) {
+		this.lop = lop;
+	}
+	public String getTenlop() {
+		return tenlop;
+	}
+	public void setTenlop(String tenlop) {
+		this.tenlop = tenlop;
+	}
+	public String getSdt() {
+		return sdt;
+	}
+	public void setSdt(String sdt) {
+		this.sdt = sdt;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getQuequan() {
+		return quequan;
+	}
+	public void setQuequan(String quequan) {
+		this.quequan = quequan;
+	}
+	public String getGhichu() {
+		return ghichu;
+	}
+	public void setGhichu(String ghichu) {
+		this.ghichu = ghichu;
+	}
+	
+
+}
